fa141185c2d1f9e3c46ad8313413322e0d626791
atulsingla82 <asingla@wgu.edu>
2017-09-14 18:19:58 -0400

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux';
+import {bindActionCreators } from 'redux';
+import {addReminder} from '../actions'
 import './App.css';
 import {Button, Icon, Row, Input} from 'react-materialize'
 
 
 class App extends Component {
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+
+    text : ''
+    }
+  }
+
+  addReminder(){
+
+    console.log('this.state', this.state)
+  }
+
   render() {
 
     return (
     	<div>
     	<h3> Reminder Pro </h3>
     	<Row>
-		<Input s={6} label="I have to ..." />
+      
+		<Input  s={6} 
+    label="I have to ..." 
+    onChange= {event => this.setState({text:event.target.value})} />
 		
 		
 		
          </Row>
-    	<Button waves='light'>
+    	<Button 
+      onClick={()=> this.addReminder()}
+      waves='light'>
 		<Icon>note_add</Icon>
 	</Button>
 	</div>
@@ -24,4 +46,9 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch){
+
+  return bindActionCreators({addReminder}, dispatch)
+}
+
+export default connect (null, mapDispatchToProps) (App);
